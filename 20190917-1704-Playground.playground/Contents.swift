078d075f55b4e5d7bb9ac35ca9dff009bbf4a1db
loyihsu <loyi.hsu@gmail.com>
2021-04-07 02:15:33 +0800

    Update to improve code clarity and handle errors.

--- 20190917-1704-Playground.playground/Contents.swift
+++ 20190917-1704-Playground.playground/Contents.swift
@@ -1,42 +1,44 @@
-import Cocoa
+import Foundation
 
 /// Track the changes of Facebook page likes with mere html access to the page
-// Facebook would block your access when you access it too often. This code attempts to keep the function going with a Timer.
+/// Facebook would block your access when you access it too often. This code attempts to keep the function going with a Timer.
+
+typealias Minutes = Double
 
 // MARK: - Setup
 let trackTitle = "柯文哲"
 let theURL = "https://www.facebook.com/DoctorKoWJ/" // https://www.facebook.com/THE_PAGE_TO_TRACK or https://zh-tw.facebook.com/THE_PAGE_TO_TRACK would both work.
-let interval: Double = 5 // Minutes
+let interval: Minutes = 5
 
 // MARK: - Initialise
 var prev = ""
 var currentMax = 0
 
-print("Facebook Page Likes Quicktrack: \(trackTitle) (Loyiworks)")  // Announce title
+print("Facebook Page Likes Tracker: \(trackTitle) (Loyiworks)")  // Announce title
 
 //MARK: - Model
 
 func getTheInfo() {
-    do {
-        if let url = validURL(theURL) {
-            let html = try String.init(contentsOf: url)
-            if let p = html.range(of: #"<div>(\d|,)+ 人說這讚<\/div>"#, options: .regularExpression) {
-                if let q = html[p].firstIndex(of: "人") {
-                    let s = html[html.index(p.lowerBound, offsetBy: 5)..<html.index(before: q)]
-                    let t = Int(s.components(separatedBy: ",").joined())!
-                    if currentMax == 0 { currentMax = t }       // update current max only when current Max is 0
-                    
-                    print("\(getCurrrentDateTime()) - \(s) (\(t - currentMax))")
-                }
+    if let url = validURL(theURL), let html = try? String.init(contentsOf: url) {
+        if let p = html.range(of: #"<div>(\d|,)+ 人說這讚<\/div>"#, options: .regularExpression) {
+            if let q = html[p].firstIndex(of: "人") {
+                let s = html[html.index(p.lowerBound, offsetBy: 5)..<html.index(before: q)]
+                let t = Int(s.components(separatedBy: ",").joined())!
+                if currentMax == 0 { currentMax = t }       // update current max only when current Max is 0
+                print("\(getCurrrentDateTime()) - \(s) (\(t - currentMax))")
+            } else {
+                print("Error: Message not found.")
             }
+        } else {
+            print("Error: Specified page is not valid.")
         }
-    } catch {
-        // Error handling
+    } else {
+        print("Error: Not a valid URL.")
     }
 }
 
 // MARK: - Run
-let t = Timer.init(timeInterval: interval*60, repeats: true) { (t) in
+let t = Timer(timeInterval: interval*60, repeats: true) { (t) in
     getTheInfo()
 }
 
--- 20190917-1704-Playground.playground/Sources/helpers.swift
+++ 20190917-1704-Playground.playground/Sources/helpers.swift
@@ -1,27 +1,19 @@
 import Foundation
 
+let formatter = DateFormatter()
+
 public func getCurrrentDateTime() -> String {
     let date = Date()
-    let calendar = Calendar.current
-    
-    let dat     = calendar.component(.day, from: date)
-    let hours   = calendar.component(.hour, from: date)
-    let minutes = calendar.component(.minute, from: date)
-    let seconds = calendar.component(.second, from: date)
-    
-    return "\(dat) - \(hours):\((minutes < 10) ? "0\(minutes)" : "\(minutes)"):\((seconds < 10) ? "0\(seconds)": "\(seconds)")"
+    formatter.dateFormat = "YYYY/MM/dd (HH:mm:ss)"
+    return "\(formatter.string(from: date))"
 }
 
 public func validURL(_ url: String) -> URL? {
     let format = "https://zh-tw.facebook.com/"
-    
     if url.components(separatedBy: format).count > 1 {
         return URL(string: url)
     }
-    
     let k = url.components(separatedBy: "https://www.facebook.com/")
     if  k.count > 1 { return URL(string: format+k.joined()) }
-    
     return nil
 }
- 
